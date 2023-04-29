c3c30694ea1502dd71019617b501e1d6f5b7756c
takah29 <takah0209@gmail.com>
2023-04-30 02:04:09 +0900

    修正

--- 3d_reconstruct_from_multi_view.py
+++ 3d_reconstruct_from_multi_view.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from lib.camera import Camera
-from lib.camera_calibration import orthographic_self_calibration
+from lib.camera_calibration import orthographic_self_calibration, paraperspective_self_calibration
 from lib.utils import sample_hemisphere_points, set_points1
 from lib.visualization import init_3d_ax, plot_2d_points, plot_3d_basis, plot_3d_points
 
@@ -11,7 +11,7 @@ def main():
     np.random.seed(123)
 
     f = 1.0
-    image_num = 5
+    image_num = 10
 
     # カメラの設定
     camera_pos = sample_hemisphere_points(image_num, 5)
@@ -37,7 +37,7 @@ def main():
     for camera in cameras:
         camera_poses.append((camera.get_pose()))
 
-    X_, R_ = orthographic_self_calibration(*x_list)
+    X_, R_ = paraperspective_self_calibration(*x_list)
 
     # 3次元点の表示
     ax = init_3d_ax()
