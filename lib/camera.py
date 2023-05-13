020ea9510731d7fe7437b242adfda7eba0712328
takah29 <takah0209@gmail.com>
2023-05-13 22:23:37 +0900

    カメラクラスのコンストラクタを修正

--- lib/camera.py
+++ lib/camera.py
@@ -4,23 +4,21 @@ from .utils import unit_vec
 
 
 class Camera:
-    def __init__(self, origin=(0.0, 0.0, 0.0), target=(0.0, 0.0, 1.0), f=1.0):
-        origin = np.asarray(origin)
-        target = np.asarray(target)
-        self.o = origin
-        self.d = unit_vec(target - origin)
-        self.f = f
+    def __init__(self, R, t, K=np.eye(3)):
+        self._R = R
+        self._t = t
+        self._K = K
 
-    def get_camera_matrix(self, f0):
-        return self._calc_camera_matrix(f0)
+    def get_camera_matrix(self):
+        return self._K @ np.hstack([self._R.T, -self._R.T @ self._t[:, np.newaxis]])
 
     def get_pose(self):
-        return self._calc_pose()
+        return self._R, self._t
 
-    def project_points(self, X, f0, method="perspective"):
+    def project_points(self, X, method="perspective"):
         """3次元点を画像面に投影する"""
         if method == "perspective":
-            res = self._perspective_projection(X, f0)
+            res = self._perspective_projection(X)
         elif method == "orthographic":
             res = self._orthographic_projection(X)
         else:
@@ -28,29 +26,37 @@ class Camera:
 
         return res
 
-    def _calc_camera_matrix(self, f0):
-        R, t = self._calc_pose()
-        return np.diag((self.f, self.f, f0)) @ np.hstack([R.T, -R.T @ t[:, np.newaxis]])
-
-    def _calc_pose(self):
-        world_top = np.array([1.0, 0.0, 0.0])
-        camera_z = self.d
-        camera_y = unit_vec(np.cross(camera_z, world_top))  # camera right
-        camera_x = unit_vec(np.cross(camera_y, camera_z))  # camera up
-        R = np.vstack((camera_x, camera_y, camera_z)).T
-        t = self.o
-        return R, t
-
-    def _perspective_projection(self, X, f0):
+    def _perspective_projection(self, X):
         X_ext = np.hstack((X, np.ones((X.shape[0], 1))))
-        Xproj = X_ext @ self.get_camera_matrix(f0).T
+        Xproj = X_ext @ self.get_camera_matrix().T
         return Xproj[:, :2] / Xproj[:, -1:]
 
     def _orthographic_projection(self, X):
         X_ext = np.hstack((X, np.ones((X.shape[0], 1))))
-        R, t = self._calc_pose()
-        Xproj = X_ext @ np.hstack([R.T, -R.T @ t[:, np.newaxis]]).T
-        return Xproj[:, :2]
+        R, t = self.get_pose()
+        X_ext_ = X_ext @ np.hstack([R.T, -R.T @ t[:, np.newaxis]]).T
+        return X_ext_[:, :2]
+
+    @staticmethod
+    def _calc_external_params(origin, direction):
+        world_top = np.array([1.0, 0.0, 0.0])
+        camera_z = direction
+        camera_y = unit_vec(np.cross(camera_z, world_top))  # camera right
+        camera_x = unit_vec(np.cross(camera_y, camera_z))  # camera up
+        R = np.vstack((camera_x, camera_y, camera_z)).T
+        t = origin
+        return R, t
+
+    @staticmethod
+    def create(origin=(0.0, 0.0, 0.0), target=(0.0, 0.0, 1.0), f=1.0, f0=1.0):
+        origin = np.asarray(origin)
+        target = np.asarray(target)
+        direction = unit_vec(target - origin)
+
+        R, t = Camera._calc_external_params(origin, direction)
+        K = np.diag((f, f, f0))
+
+        return Camera(R, t, K)
 
 
 if __name__ == "__main__":
@@ -60,13 +66,13 @@ if __name__ == "__main__":
     X = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
     # case1
-    camera = Camera((0, 0, -1), (0, 0, 1), f=1)
-    Xproj = camera.project_points(X, 1.0)
+    camera = Camera.create((0, 0, -1), (0, 0, 1), f=1)
+    Xproj = camera.project_points(X)
     nptest.assert_array_almost_equal(Xproj, np.array([[0, 0], [1, 0], [0, 1], [0, 0]]))
 
     # case2
-    camera = Camera((0, -1, 0), (0, 1, 0), f=1)
-    Xproj = camera.project_points(X, 1.0)
+    camera = Camera.create((0, -1, 0), (0, 1, 0), f=1)
+    Xproj = camera.project_points(X)
     nptest.assert_array_almost_equal(Xproj, np.array([[0, 0], [1, 0], [0, 0], [0, -1]]))
 
     print("Passed all tests.")
