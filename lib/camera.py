4f753ca4c2309b89a89057806b3ae1e624ebb51e
takah29 <takah0209@gmail.com>
2023-05-20 05:40:21 +0900

    型ヒント追加

--- lib/camera.py
+++ lib/camera.py
@@ -1,21 +1,22 @@
 import numpy as np
+import numpy.typing as npt
 
 from .utils import unit_vec
 
 
 class Camera:
-    def __init__(self, R, t, K=np.eye(3)):
+    def __init__(self, R: npt.NDArray, t: npt.NDArray, K: npt.NDArray = np.eye(3)):
         self._R = R
         self._t = t
         self._K = K
 
-    def get_camera_matrix(self):
+    def get_camera_matrix(self) -> npt.NDArray:
         return self._K @ np.hstack([self._R.T, -self._R.T @ self._t[:, np.newaxis]])
 
-    def get_parameters(self):
+    def get_parameters(self) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
         return self._K, self._R, self._t
 
-    def project_points(self, X, method="perspective"):
+    def project_points(self, X: npt.NDArray, method: str = "perspective") -> npt.NDArray:
         """3次元点を画像面に投影する"""
         if method == "perspective":
             res = self._perspective_projection(X)
@@ -26,29 +27,40 @@ class Camera:
 
         return res
 
-    def _perspective_projection(self, X):
+    def _perspective_projection(self, X: npt.NDArray) -> npt.NDArray:
         X_ext = np.hstack((X, np.ones((X.shape[0], 1))))
         Xproj = X_ext @ self.get_camera_matrix().T
+
         return Xproj[:, :2] / Xproj[:, -1:]
 
-    def _orthographic_projection(self, X):
+    def _orthographic_projection(self, X: npt.NDArray) -> npt.NDArray:
         X_ext = np.hstack((X, np.ones((X.shape[0], 1))))
         _, R, t = self.get_parameters()
         X_ext_ = X_ext @ np.hstack([R.T, -R.T @ t[:, np.newaxis]]).T
+
         return X_ext_[:, :2]
 
     @staticmethod
-    def _calc_external_params(origin, direction):
+    def _calc_external_params(
+        origin: npt.NDArray,
+        direction: npt.NDArray,
+    ) -> tuple[npt.NDArray, npt.NDArray]:
         world_top = np.array([1.0, 0.0, 0.0])
         camera_z = direction
         camera_y = unit_vec(np.cross(camera_z, world_top))  # camera right
         camera_x = unit_vec(np.cross(camera_y, camera_z))  # camera up
         R = np.vstack((camera_x, camera_y, camera_z)).T
         t = origin
+
         return R, t
 
     @staticmethod
-    def create(origin=(0.0, 0.0, 0.0), target=(0.0, 0.0, 1.0), f=1.0, f0=1.0):
+    def create(
+        origin: npt.NDArray | tuple[float, float, float] = (0.0, 0.0, 0.0),
+        target: npt.NDArray | tuple[float, float, float] = (0.0, 0.0, 1.0),
+        f: float = 1.0,
+        f0: float = 1.0,
+    ) -> "Camera":
         origin = np.asarray(origin)
         target = np.asarray(target)
         direction = unit_vec(target - origin)
